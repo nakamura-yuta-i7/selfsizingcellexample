68dbac27397476af6f4be279d73d16b7094c2d4c
中村祐太 <yuta@macbook-pro.local>
2015-09-17 22:54:43 +0900

    first commit

---
+++ SelfSizingCellExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelfSizingCellExample
+//
+//  Created by 中村祐太 on 2015/09/17.
+//  Copyright (c) 2015年 中村祐太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
